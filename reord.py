94a0680979f0909ee568fc9d4711c0e99a549601
RithinChand2004 <97657716+RithinChand2004@users.noreply.github.com>
2023-08-02 10:49:32 +0530

    Update reord.py

--- reord.py
+++ reord.py
@@ -10,16 +10,16 @@ def record_audio(filename, duration=5):
     fs = 44100  # Sample rate
     seconds = duration  # Duration of recording
     
-    # devices = sd.query_devices()
-    # for i, device in enumerate(devices):
-    #     print(i, device['name'])
+    devices = sd.query_devices()
+    for i, device in enumerate(devices):
+        print(i, device['name'])
 
-    # input_device_index = 0  # Replace with the index of your desired input device
-    # audio_data = sd.rec(int(fs * seconds), samplerate=fs, channels=2, device=input_device_index)
+    input_device_index = 1  # Replace with the index of your desired input device
+    audio_data = sd.rec(int(fs * seconds), samplerate=fs, channels=2, device=input_device_index)
 
 
     st.write("Recording...")
-    audio_data = sd.rec(int(fs * seconds), samplerate=fs, channels=2)
+    # audio_data = sd.rec(int(fs * seconds), samplerate=fs, channels=2)
     sd.wait()  # Wait until recording is finished
 
     # with wave.open(filename, "wb") as wf:
